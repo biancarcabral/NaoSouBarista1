53ede454a356e02689a6d744ced7f5a97f991570
BiancaCabral <biancarodriguescabral@gmail.com>
2024-10-29 15:23:21 -0300

    Script and Label Select Creation
    criação do arquivo java script e da caixa de seleção dentro do html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const metodoSelect = document.getElementById("metodo");
+const moedorSelect = document.getElementById("moedor");
+const resultado = document.getElementById("resultado");
+
+function atualizarResultado() {
+    const metodo = metodoSelect.options[metodoSelect.selectedIndex].text;
+    const moedor = moedorSelect.options[moedorSelect.selectedIndex].text;
+    resultado.textContent = `Método de preparo: ${metodo} | Moedor: ${moedor}`;
+}
+
+metodoSelect.addEventListener("change", atualizarResultado);
+moedorSelect.addEventListener("change", atualizarResultado);
